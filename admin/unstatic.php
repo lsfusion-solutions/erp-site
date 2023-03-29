b45391b162778f257ad47b017841ae6fbaa883b8
aramanenka <sasha.romanenko@gmail.com>
2023-03-29 13:39:32 +0300

    Added cross origin header

--- admin/unstatic.php
+++ admin/unstatic.php
@@ -1,5 +1,7 @@
 <?php
 
+    header("Access-Control-Allow-Origin: *");
+
 $allowerEmails = array(
     "info@lsfusion.org", "elena@mite.club", // для MyCompany
     "sasha.romanenko@gmail.com", "office@luxsoft.by", "info@fit.ru",//для сайта ERP
